5e170c6a33e803df92029902ae2aa04ae9c335d6
Yusuf Rizal <yusufrizalh@gmail.com>
2019-11-27 14:13:47 +0700

    javafx-scene-builder-dashboard

--- src/sample/Dashboard.java
+++ src/sample/Dashboard.java
@@ -1,4 +1,52 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class Dashboard {
+    // inisialisasi komponen dalam layout dashboard
+    @FXML
+    private Button btn_lihat_data;
+    @FXML
+    private Button btn_tambah_data;
+    @FXML
+    private Button btn_ubah_data;
+    @FXML
+    private Button btn_hapus_data;
+
+    @FXML
+    private AnchorPane anchorPaneLayout;
+
+    // method untuk memanggil layout
+    private void panggilLayout(String layout) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource(layout + ".fxml"));
+        Stage stage = new Stage();
+        stage.setScene(new Scene(root));
+        stage.show();
+    }
+
+    // method-method untuk masing-masing button
+    void lihatData(ActionEvent event) throws IOException {
+        panggilLayout("lihat_data");
+    }
+
+    void tambahData(ActionEvent event) throws IOException {
+        panggilLayout("tambah_data");
+    }
+
+    void ubahData(ActionEvent event) throws IOException {
+        panggilLayout("ubah_data");
+    }
+
+    void hapusData(ActionEvent event) throws IOException {
+        panggilLayout("hapus_data");
+    }
 }
--- src/sample/Login.java
+++ src/sample/Login.java
@@ -2,11 +2,17 @@ package sample;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 import javafx.stage.Window;
+
+import java.io.IOException;
 
 public class Login {
     // inisialisasi komponen dalam layout
@@ -17,7 +23,7 @@ public class Login {
     @FXML
     private Button btn_login;
 
-    public void prosesLogin(ActionEvent event) {
+    public void prosesLogin(ActionEvent event) throws IOException {
         Window window = btn_login.getScene().getWindow();
 
         // validasi bahwa email dan password tidak boleh kosong
@@ -45,8 +51,17 @@ public class Login {
             tampilkanAlert(Alert.AlertType.INFORMATION, window,
                     "LOGIN BERHASIL", "Selamat Datang: " + email);
             // diarahkan ke dashboard
-            // memangil form dashboard
+            // memangil layout dashboard
+            panggilLayout("dashboard");
         }
+    }
+
+    // method untuk memanggil layout
+    private void panggilLayout(String layout) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource(layout + ".fxml"));
+        Stage stage = new Stage();
+        stage.setScene(new Scene(root));
+        stage.show();
     }
 
     // method untuk menampilkan Alert
